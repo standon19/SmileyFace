c33c8e116d8aa1678e12978941bfac33766aeeb6
standon19 <42978500+standon19@users.noreply.github.com>
2018-09-12 14:06:58 -0400

    Add files via upload

---
+++ src/smile/SmileyFace.java
@@ -0,0 +1,19 @@
+package smile;
+/**
+ * 
+ * @author Samir
+ *smiley face
+ */
+
+
+public class SmileyFace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("\\\\\\\\\\\\");
+System.out.println("| * * |");
+System.out.println("|  *  |");
+System.out.println("| \\_/ |");
+System.out.print("\\ _ _ /");	}
+
+}
